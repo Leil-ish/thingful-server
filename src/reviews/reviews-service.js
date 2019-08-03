c63aa359f7fcc119d1fb4769e3db1683975b1922
Leila Anderson <leilacanderson@gmail.com>
2019-08-03 13:13:22 -0500

    secured login

--- src/reviews/reviews-service.js
+++ src/reviews/reviews-service.js
@@ -45,13 +45,21 @@ const ReviewsService = {
   },
 
   serializeReview(review) {
+    const {user} = review
     return {
       id: review.id,
       rating: review.rating,
       text: xss(review.text),
       thing_id: review.thing_id,
       date_created: review.date_created,
-      user: review.user || {},
+      user: {
+        id: user.id,
+        user_name: user.user_name,
+        full_name: user.full_name,
+        nickname: user.nickname,
+        date_created: new Date(user.date_created),
+        date_modified: new Date(user.date_modified) || null
+      },
     }
   }
 }
