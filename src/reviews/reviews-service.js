93134f95ff4830fef7e0c1bffae4955b38708349
Leila Anderson <leilacanderson@gmail.com>
2019-08-03 16:33:26 -0500

    user registration added

--- src/reviews/reviews-service.js
+++ src/reviews/reviews-service.js
@@ -3,33 +3,34 @@ const xss = require('xss')
 const ReviewsService = {
   getById(db, id) {
     return db
-      .from('thingful_reviews AS rev')
+      .from('thingful_reviews AS comm')
       .select(
-        'rev.id',
-        'rev.rating',
-        'rev.text',
-        'rev.date_created',
-        'rev.thing_id',
+        'comm.id',
+        'comm.text',
+        'comm.date_created',
+        'comm.thing_id',
         db.raw(
-          `row_to_json(
-            (SELECT tmp FROM (
-              SELECT
-                usr.id,
-                usr.user_name,
-                usr.full_name,
-                usr.nickname,
-                usr.date_created,
-                usr.date_modified
-            ) tmp)
+          `json_strip_nulls(
+            row_to_json(
+              (SELECT tmp FROM (
+                SELECT
+                  usr.id,
+                  usr.user_name,
+                  usr.full_name,
+                  usr.nickname,
+                  usr.date_created,
+                  usr.date_modified
+              ) tmp)
+            )
           ) AS "user"`
         )
       )
       .leftJoin(
         'thingful_users AS usr',
-        'rev.user_id',
+        'comm.user_id',
         'usr.id',
       )
-      .where('rev.id', id)
+      .where('comm.id', id)
       .first()
   },
 
@@ -45,13 +46,12 @@ const ReviewsService = {
   },
 
   serializeReview(review) {
-    const {user} = review
+    const { user } = review
     return {
       id: review.id,
-      rating: review.rating,
       text: xss(review.text),
       thing_id: review.thing_id,
-      date_created: review.date_created,
+      date_created: new Date(review.date_created),
       user: {
         id: user.id,
         user_name: user.user_name,
